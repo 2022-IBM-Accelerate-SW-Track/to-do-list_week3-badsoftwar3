8ca3229291bd26318ddad7bfb77defb146444b14
jerry Odigie <odigiejerry@gmail.com>
2022-06-25 02:46:05 -0400

    Changes fixed

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -24,7 +24,8 @@ afterEach(() => {
   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
-  const dueDate = "06/24/2022";
+  const dueDate = "06/24/2023";
+  const dueDate2 = "07/23/2023";
   
 
   fireEvent.change(inputTask, { target: { value: "History Test"}});
@@ -32,10 +33,9 @@ afterEach(() => {
   fireEvent.click(element);
 
   fireEvent.change(inputTask, { target: { value: "History Test"}});
-  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.change(inputDate, { target: { value: dueDate2}});
   fireEvent.click(element);
-  const check = screen.getAllByText(/History Test/i)
-  expect(check.length).toBe(1);
+  screen.getByText(/History Test/i);
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
@@ -43,7 +43,7 @@ afterEach(() => {
 
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
-  const dueDate = "06/24/2022";
+  const dueDate = "06/24/2023";
 
   
   fireEvent.change(inputDate, { target: { value: dueDate}});
@@ -73,7 +73,7 @@ afterEach(() => {
   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
-  const dueDate = "06/24/2022";
+  const dueDate = "06/24/2023";
 
   fireEvent.change(inputTask, { target: { value: "History Test"}});
   fireEvent.change(inputDate, { target: { value: dueDate}});
@@ -95,7 +95,7 @@ afterEach(() => {
   const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
   const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
   const element = screen.getByRole('button', {name: /Add/i});
-  const dueDate = "06/24/2022";
+  const dueDate = "06/24/2023";
 
   fireEvent.change(inputTask, { target: { value: "History Test"}});
   fireEvent.change(inputDate, { target: { value: dueDate}});
