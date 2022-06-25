ca9c477012f32ae4158eb73a9ed5cc2d6dc42c6c
jerry Odigie <odigiejerry@gmail.com>
2022-06-25 03:34:46 -0400

    Hopefully this is last update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,7 +29,7 @@ class Home extends Component {
   // updates the state with the new list.
   addTodo = (todo) => {
     const exists = this.state.todos.find(t => t.content === todo.content );
-    if (exist || todo.due === "Invalid Date" || todo.due === null)
+    if (exists || todo.due === "Invalid Date" || todo.due === null)
     {return}
 
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
